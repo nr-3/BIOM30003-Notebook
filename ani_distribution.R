856c0dd63d3fc8e23de15c180fd68299949b9ce9
nraj003 <126905527+nraj003@users.noreply.github.com>
2025-03-18 13:16:34 +1100

    updated ani distribution

--- ani_distribution.R
+++ ani_distribution.R
@@ -14,7 +14,7 @@ ani_distribution <- function(se, genome_file, predictor, plot = T) {
   case <- which(colnames(tibble::as_tibble(se@colData@listData)) == predictor)
   
   # Validate that the contig/predictor exists
-  if(contig == 0 | case == 0) {
+  if(length(contig) == 0 | length(case) == 0) {
     stop('Provide a valid predictor or genome file')
   }
   
@@ -29,14 +29,22 @@ ani_distribution <- function(se, genome_file, predictor, plot = T) {
     return(plot_data)
   }
   
+  custom_colors <- c('#a6cee3','#1f78b4','#b2df8a','#33a02c','#fb9a99','#e31a1c','#fdbf6f','#ff7f00','#cab2d6','#6a3d9a','#b15928',
+                     '#8dd3c7','#bebada','#fb8072','#80b1d3','#fdb462','#b3de69','#fccde5','#d9d9d9','#bc80bd','#ccebc5')
   
-  plot <- ggplot2::ggplot(plot_data, aes(x=predictor,y=ani)) +
+  custom_colors <- custom_colors[1:length(unique(plot_data$predictor))]
+  
+  
+  plot <- ggplot2::ggplot(plot_data, aes(x=predictor,y=ani, fill = predictor)) +
     geom_boxplot() +
     ggplot2::xlab(label = predictor) +
-    ggplot2::scale_y_continuous(name = 'ANI', limits = c(97,100))
+    ggplot2::scale_fill_manual(values = custom_colors) +
+    ggplot2::scale_y_continuous(name = 'ANI', limits = c(97,100)) +
+    theme_classic() +
+    theme(legend.position='none')
   
   return(plot)
 
 }
 
-ANI_distribution(se, 'GCF_025147485.1', 'Case_status', plot=T)
+ani_distribution(se, 'GCF_025147485.1', 'Case_status', plot=T)
