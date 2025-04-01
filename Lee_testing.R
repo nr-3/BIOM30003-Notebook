4419b289c5027102bd4e566d400781f2da95b006
nraj003 <126905527+nraj003@users.noreply.github.com>
2025-04-01 17:03:19 +1100

    updated comments

--- Lee_testing.R
+++ Lee_testing.R
@@ -2,31 +2,43 @@
 meta_data <- readr::read_csv('C:/Users/nraja/BIOM30003-Notebook/Lee/lee_PRJEB43119_metadata.csv')
 lee_data <- readr::read_tsv('C:/Users/nraja/BIOM30003-Notebook/Lee/lee_sylph_profile_gtdb-r220-c200-dbv1.tsv/lee_sylph_profile_gtdb-r220-c200-dbv1.tsv')
 
+# File names don't match between meta data and sylph file
+# Join table created to match up file names
 join_table <- readr::read_tsv("C:/Users/nraja/Downloads/filereport_read_run_PRJEB43119.tsv")
 
+# Removing everything before the 6th `/` and after the `;`
 join_table$fastq_ftp <- gsub('^(.*?/){6}','',join_table$fastq_ftp)
 join_table$fastq_ftp <- gsub(';.*','',join_table$fastq_ftp)
 
+# Format as it is in the sylph files
 join_table$fastq_ftp <- paste0('./',join_table$fastq_ftp)
 join_table$sample_title <- gsub(' wgs','',join_table$sample_title)
 
+# Add the formatted file names back into the meta data
 meta_data <- join_table |>
   select(fastq_ftp,sample_title) |>
   inner_join(meta_data, by = join_by(sample_title == sample_id))
 
+# Read profile sylph file
 se_lee <- read_sylph('C:/Users/nraja/BIOM30003-Notebook/Lee/lee_sylph_profile_gtdb-r220-c200-dbv1.tsv/lee_sylph_profile_gtdb-r220-c200-dbv1.tsv', meta_data)
 se_lee <- filter_by_presence(se_lee, min_nonzero = 30)
 
+# PCA plot check
 pca_biplot(se_lee, 'antibiotics_current_use')
 
-se_lee_q <- read_sylph('C:/Users/nraja/BIOM30003-Notebook/Lee/lee_sylph_query_gtdb_220_id99.tsv\lee_sylph_query_gtdb_220_id99.tsv')
+# Read query sylph file
+se_lee_q <- read_sylph('C:/Users/nraja/BIOM30003-Notebook/Lee/lee_sylph_query_gtdb_220_id99.tsv/lee_sylph_query_gtdb_220_id99.tsv')
+se_lee_q <- filter_by_presence(se_lee, min_nonzero = 30)
 
 design <- as.formula(" ~ antibiotics_current_use")
 
+# Fit using zero inflated beta
 fit <- glmZiBFit(se_lee, design)
+fit_q <- glmZiBFit(se_lee_q, design)
 
 taxonomy <- read_taxonomy(example_taxonomy_path)
 
+# Volcano plot check
 volcano_plot(se = se_lee, fit = fit, predictor = 'antibiotics_current_use')
 head(top_hits(fit, alpha = 1))
 
--- pca_biplot.R
+++ pca_biplot.R
@@ -1,40 +1,53 @@
-# PCA plot
+#' Perform PCA and Generate a Biplot
+#'
+#' This function performs Principal Component Analysis (PCA) on a SummarizedExperiment object
+#' and generates a PCA biplot colored by a specified phenotype.
+#'
+#' @param se A `SummarizedExperiment` object containing expression data.
+#' @param predictor A character string specifying the phenotype by which the plot is coloured
+#' @param plot A logical value indicating whether to return the PCA plot (`TRUE`) or PCA data (`FALSE`). Default is `TRUE`.
+#'
+#' @return If `plot = TRUE`, returns a `ggplot2` PCA plot. If `plot = FALSE`, returns a tibble with PCA results.
 
 pca_biplot <- function(se, predictor, plot = T) {
 
   asy<-SummarizedExperiment::assay(se)
+  
+  # PCA
   PC <- prcomp(t(as.matrix(asy)), scale. = T)
   
+  # Calculate the percent variance from the first two PC
   pve <- PC$sdev^2/sum(PC$sdev^2)
   xLab <- paste0("PC1 (",round(pve[1]*100,2),"%)")
   yLab <- paste0("PC2 (",round(pve[2]*100,2),"%)")
   
-  #Format input data
+  #Format PC data
   if(is.null(rownames(PC$x))){
     rownames(PC$x)<-1:nrow(PC$x)
   }
-  
   dat <- tibble::as_tibble(PC$x)
   
+  # Access the phenotype by which the plot is coloured
   col <- which(colnames(tibble::as_tibble(se@colData@listData)) == predictor)
   col <- se@colData@listData[[col]]
-  
   dat$pred <- col
   
+  # Return plot data if requested
   if(!plot) {
     return(dat)
   }
   
   # Create a PCA plot
-  plot <- ggplot(
+  plot <- ggplot2::ggplot(
     data = dat, 
     mapping = aes(x=PC1, y=PC2, color = pred)
     ) +
-    geom_point()+
-    theme_bw()+
+    ggplot2::geom_point()+
+    ggplot2::theme_bw() +
     theme_classic()+
     labs(x=xLab,y=yLab)
   
+  # Add a gradient colour if phenotype is continuous
   if(is.numeric(col)) {
     plot <- plot + scale_color_viridis_c(option = 'plasma')
   }
